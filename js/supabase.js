62d270c46be4e29127c6ae16eaefdd043f8d3b71
jkp1231 <jungkilpark@naver.com>
2026-03-15 12:33:59 +0900

    Fix Supabase initialization error

--- js/supabase.js
+++ js/supabase.js
@@ -3,6 +3,6 @@ const SUPABASE_URL = 'https://siquixdnkxrcovbppubv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpcXVpeGRua3hyY292YnBwdWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MjIzMjQsImV4cCI6MjA4OTA5ODMyNH0.4tS-OuzicJIVtGkedhsZlbI0GWf9tq57EO0Cpp5okRM';
 
 // Initialize the Supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-export { supabase };
+export { supabaseClient as supabase };
